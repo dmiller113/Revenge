0574093c9c992a918a2ccab0704136794c4a5bdd
Daniel Miller <danm1113@gmail.com>
2022-03-07 03:00:43 -0800

    Hello world rot.js style

--- src/game/configuration/constants.ts
+++ src/game/configuration/constants.ts
@@ -1,1 +1,3 @@
 export const GAME_NAME = 'Revenge';
+export const GAME_WIDTH = 80;
+export const GAME_HEIGHT = 24;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-import {GAME_NAME} from 'game/configuration/constants';
-console.dir(GAME_NAME);
+import {Display} from 'rot-js';
+import {
+  GAME_HEIGHT,
+  GAME_WIDTH,
+} from 'game/configuration/constants';
+
+const display = new Display({
+  height: GAME_HEIGHT,
+  width: GAME_WIDTH,
+});
+
+document.body.appendChild(display.getContainer());
+
+display.drawText(35, 12, "Hello World");
