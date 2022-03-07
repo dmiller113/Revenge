583900d235743fab0c678dc8cf3e840259b8d868
Daniel Miller <danm1113@gmail.com>
2022-03-07 02:12:48 -0800

    Add in favicon

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 
 app.use('/src', express.static('public', {redirect: false}))
+app.use('/favicon.ico', express.static('./favicon.ico'))
 app.use('/', express.static('template'));
 
 app.listen(port, () => {
