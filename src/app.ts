479fbdccfe71832495b3cee5fb4387476ecb44fa
Daniel Miller <danm1113@gmail.com>
2022-03-07 01:14:48 -0800

    Set up server to serve html/js

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/src', express.static('dist', {redirect: false}))
+app.use('/', express.static('template'));
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
-
